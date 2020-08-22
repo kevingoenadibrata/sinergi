718445e5bed7d70f2e8ede31afa5b65e7f404ff1
Kevin Goenadibrata <kevingoenadibrata@gmail.com>
2020-08-22 08:03:46 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
+import sha256 from 'sha256';
+import {BounceLoader} from 'react-spinners';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [image, setImage] = useState('');
+  const [hashInput, setHashInput] =useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setHashInput(e.target.value);
+  }
+  const handleClick = () =>{
+    setLoading(true);
+    axios.post('http://165.232.53.236/hash', {
+      hash: sha256(hashInput)
+    })
+    .then(function (response) {
+      console.log(response);
+      setImage(response.data);
+      setLoading(false);
+    })
+    .catch(function (error) {
+      console.log(error);
+      setLoading(false);
+    });
+  }
+
+  useState(()=>{
+    setLoading(true);
+    axios.post('http://165.232.53.236/hash', {
+      hash: sha256(hashInput)
+    })
+    .then(function (response) {
+      console.log(response);
+      setImage(response.data);
+      setLoading(false);
+    })
+    .catch(function (error) {
+      console.log(error);
+      setLoading(false);
+    });
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="infoContainer">
+        <div className="innerInfoContainer">
+          <h1>SINERGI</h1>
+          <input onChange={handleChange} value={hashInput}></input>
+          <button onClick={handleClick}>Generate</button>
+        </div>
+      </div>
+      <div className="imgContainer">
+        {loading ? <BounceLoader /> : <img src={`${image}`} className="image"/>}
+      </div>
     </div>
   );
 }
